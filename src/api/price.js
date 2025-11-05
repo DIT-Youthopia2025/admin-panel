b86a470f2d521044131baf620e4bf707797d1818
Siddharth <moonmars2au@gmail.com>
2025-11-05 07:30:19 +0000

    some commit to live

--- src/api/price.js
+++ src/api/price.js
@@ -3,10 +3,14 @@ import baseConfig from './base_url.json' assert { type: 'json' };
 
 const base_url = baseConfig.base_url;
 
+const token = localStorage.getItem("token");
 
 export const createPrice = async (data) => {
     const res = await axios.post(`${base_url}/prices/create`, data, {
-        withCredentials: true
+        withCredentials: true,
+        headers: {
+                Authorization: `Bearer ${token}`,
+        },
     })
     return res;
 }
